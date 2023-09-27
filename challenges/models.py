f6cc13fb292b20422e04f838013c488544b98319
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-27 21:57:32 +0300

    level_2, ex. a

--- challenges/models.py
+++ challenges/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from typing import TypedDict
+from datetime import datetime
 
 
 class Book(models.Model):
@@ -8,3 +10,58 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class LaptopJson(TypedDict):
+    id: int
+    brand: str
+    manufacture_year: int
+    hard_drive_capacity_gb: int
+    price_rub: int
+    stock_amount: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class Laptop(models.Model):
+    class Meta:
+        get_latest_by = 'created_at'
+        ordering = ["-created_at"]
+
+    brand = models.CharField(
+        max_length=10,
+        choices=[
+            ("LE", "Lenovo"),
+            ("DE", "Dell"),
+            ("AC", "Acer"),
+            ("AS", "Asus"),
+            ("AP", "Apple")
+        ],
+    )
+    manufacture_year = models.PositiveSmallIntegerField()
+    ram_amount_gb = models.PositiveSmallIntegerField()
+    hard_drive_capacity_gb = models.PositiveIntegerField()
+    price_rub = models.PositiveIntegerField()
+    stock_amount = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'id: {self.pk}, ' \
+               f'brand: {self.brand}, ' \
+               f'manufacture_year: {self.manufacture_year}, ' \
+               f'hard_drive_capacity_gb: {self.hard_drive_capacity_gb}, ' \
+               f'price_rub: {self.price_rub}, ' \
+               f'stock_amount: {self.stock_amount}'
+
+    def to_json(self) -> LaptopJson:
+        return {
+            "id": self.pk,
+            "brand": self.brand,
+            "manufacture_year": self.manufacture_year,
+            "hard_drive_capacity_gb": self.hard_drive_capacity_gb,
+            "price_rub": self.price_rub,
+            "stock_amount": self.stock_amount,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
--- challenges/views/level_2/a_laptops.py
+++ challenges/views/level_2/a_laptops.py
@@ -11,38 +11,92 @@
 - реализовать у модели метод to_json, который будет преобразовывать объект книги в json-сериализуемый словарь
 - по очереди реализовать каждую из вьюх в этом файле, проверяя правильность их работу в браузере
 """
-from django.http import HttpRequest, HttpResponse
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from challenges.models import Laptop
 
 
-def laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
+def valid_min_price(min_price: str | None) -> int | None:
+    try:
+        min_price = int(min_price)
+    except (ValueError, TypeError):
+        return None
+
+    return min_price if min_price >= 0 else None
+
+
+def valid_brand(brand: str | None) -> bool:
+    possible_brands_dict = {brand[1]: brand[0] for brand in Laptop.brand.field.choices}
+
+    return possible_brands_dict.get(brand, None)
+
+
+def convert_query_result_to_jsonresponse(query_result: Laptop | QuerySet | None) -> JsonResponse:
+    if query_result is None:
+        data = {'data': {}, 'errors': 'Page not found'}
+        status = 404
+
+    else:
+        status = 200
+        if type(query_result) == Laptop:
+            objects_data = query_result.to_json()
+
+        else:
+            objects_data = [object.to_json() for object in query_result]
+
+        data = {'data': objects_data}
+
+    return JsonResponse(data=data, status=status)
+
+
+def laptop_details_view(request: HttpRequest, laptop_id: int) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание ноутбука по его id.
     Если такого id нет, вернуть 404.
     """
-    pass
+    laptop = Laptop.objects.filter(id=laptop_id).first()
+
+    return convert_query_result_to_jsonresponse(query_result=laptop)
 
 
-def laptop_in_stock_list_view(request: HttpRequest) -> HttpResponse:
+def laptop_in_stock_list_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание всех ноутбуков, которых на складе больше нуля.
     Отсортируйте ноутбуки по дате добавления, сначала самый новый.
     """
-    pass
+    laptops = Laptop.objects.filter(stock_amount__gt=0).order_by('-created_at')
+
+    return convert_query_result_to_jsonresponse(query_result=laptops)
 
 
-def laptop_filter_view(request: HttpRequest) -> HttpResponse:
+def laptop_filter_view(request: HttpRequest) -> HttpResponse | JsonResponse:
     """
     В этой вьюхе вам нужно вернуть список ноутбуков с указанным брендом и указанной минимальной ценой.
     Бренд и цену возьмите из get-параметров с названиями brand и min_price.
     Если бренд не входит в список доступных у вас на сайте или если цена отрицательная, верните 403.
     Отсортируйте ноутбуки по цене, сначала самый дешевый.
     """
-    pass
+    brand = request.GET.get('brand')
+    min_price = request.GET.get('price')
+
+    min_price = valid_min_price(min_price)
+    brand = valid_brand(brand)
+
+    if not min_price and brand:
+        return HttpResponse(status=403)
+
+    laptops = Laptop.objects.filter(brand=brand, price_rub__lte=min_price)
+    return convert_query_result_to_jsonresponse(query_result=laptops)
 
 
-def last_laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
+def last_laptop_details_view(request: HttpRequest) -> HttpResponse | JsonResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание последнего созданного ноутбука.
     Если ноутбуков нет вообще, вернуть 404.
     """
-    pass
+    try:
+        last_laptop = Laptop.objects.latest()
+    except Laptop.DoesNotExist:
+        return HttpResponse(status=404)
+
+    return convert_query_result_to_jsonresponse(query_result=last_laptop)
